f2eac03a629ccb5b78fbed371136e9437230cca2
daniil.zelenskyi <daniil.zelenskyi@anvileight.com>
2025-12-24 21:59:41 +0200

    1234

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ document.getElementById("contactForm")
   alert("Повідомлення надіслано!");
   e.target.reset();
 });
+
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+    const target = e.currentTarget.getAttribute('href');
+    if (target) {
+      const element = document.querySelector(target);
+      element?.scrollIntoView({ behavior: 'smooth' });
+    }
+  });
+});
