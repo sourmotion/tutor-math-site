6c692002b854449aa5036958109289ad44a60bd7
daniil.zelenskyi <daniil.zelenskyi@anvileight.com>
2025-12-25 01:39:32 +0200

    1234

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,29 @@
 document.getElementById("contactForm")
-.addEventListener("submit", e => {
-  event.preventDefault();
-  const name = form.elements['name'].value.trim();
-  const email = form.elements['email'].value.trim();
-  const message = form.elements['message'].value.trim();
-  const emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,6}$/i;
-  if (!emailPattern.test(email)) {
-    alert('Будь ласка, введіть коректний Email.');
-    return;
-  }
-  if (name === "" || message === "") {
-    alert('Будь ласка, заповніть усі поля форми.');
-    return;
-  }
-  alert("Повідомлення надіслано!");
-  e.target.reset();
-});
+  .addEventListener("submit", e => {
+    e.preventDefault();
+
+    const form = e.target;
+
+    const name = form.elements['name'].value.trim();
+    const email = form.elements['email'].value.trim();
+    const message = form.elements['message'].value.trim();
+
+    const emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,6}$/i;
+
+    if (!emailPattern.test(email)) {
+      alert('Будь ласка, введіть коректний Email.');
+      return;
+    }
+
+    if (name === "" || message === "") {
+      alert('Будь ласка, заповніть усі поля форми.');
+      return;
+    }
+
+    alert("Повідомлення надіслано!");
+    form.reset();
+  });
+
 
 document.querySelectorAll('a[href^="#"]').forEach(link => {
   link.addEventListener('click', e => {
