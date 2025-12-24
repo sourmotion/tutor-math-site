ee1c019dcb40bb2c5b0fc514a9d59371baa11ff2
daniil.zelenskyi <daniil.zelenskyi@anvileight.com>
2025-12-25 00:58:15 +0200

    1234

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,18 @@
 document.getElementById("contactForm")
 .addEventListener("submit", e => {
-  e.preventDefault();
+  event.preventDefault();
+  const name = form.elements['name'].value.trim();
+  const email = form.elements['email'].value.trim();
+  const message = form.elements['message'].value.trim();
+  const emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,6}$/i;
+  if (!emailPattern.test(email)) {
+    alert('Будь ласка, введіть коректний Email.');
+    return;
+  }
+  if (name === "" || message === "") {
+    alert('Будь ласка, заповніть усі поля форми.');
+    return;
+  }
   alert("Повідомлення надіслано!");
   e.target.reset();
 });
